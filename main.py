28898e1878a67eb217777cb82a8a59bc263cadfd
maksgordt <intomixins@gmail.com>
2023-08-31 12:07:14 +0400

    add pictures

--- main.py
+++ main.py
@@ -11,28 +11,63 @@ load_dotenv()
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 chat_id = -1001966702354
+# chat_id = -1001855558283
+
+
+@bot.message_handler(commands=['id'])
+def get_id(message):
+    print(message.message_thread_id)
 
 
 def send_event_map():
     response = requests.get('https://eneik.ge/event/telegram').json()
-    mes = f'{COMMON_TEXT}\n\n'
+    bot.send_message(chat_id, COMMON_TEXT, message_thread_id=2, parse_mode='html')
+    no_tag = ''
+    DAILY = {
+        '#каждый_понедельник': '',
+        '#каждый_вторник': '',
+        '#каждую_среду': '',
+        '#каждый_четверг': '',
+        '#каждую_пятницу': '',
+        '#каждую_субботу': '',
+        '#каждое_воскресенье': '',
+    }
     for event in response:
-        status = True if 'subtitles' in event['description'] else False
+        status = True if 'dub' in event['description'] else False
         url = f'https://eneik.ge/event/{event["id"]}'
         date, time = event['dateStart'].split(' ')
-        date = f"#{date.split('-')[2]}{DCT[date.split('-')[1]]}{date.split('-')[0]}"
+        day, month, year = date.split('-')[2], DCT[date.split('-')[1]], date.split('-')[0]
+        if int(day) < 10:
+            day = day[1]
+        date = f"#{day}{month}{year}"
         time = f"{time.split(':')[0]}:{time.split(':')[1]}"
+        name, description, price = event["name"], event["description"], event["price"]
+        flag = False
+        result = ''
         if status:
-            mes += f'🎥{date} в {time} будет показ фильма {event["name"]}\n'
+            result += f'🎥{date}\n{time} показ фильма <b>{event["name"]}</b>\n\n'
         else:
-            mes += f'{date} в {time} будет {event["name"]}\n'
-        mes += f'Описание: {event["description"]}\n'
-        mes += f'Цена: {event["price"]} лари\n'
-        mes += f'{url}\n\n'
-    mes += FINISH_TEXT
-    bot.send_message(chat_id, mes, reply_to_message_id=2)
+            result += f'{date}\n{time} <b>{event["name"]}</b>\n'
+            result += f'{url}\n'
+            result += f'Описание: {description}\n'
+            result += f'Цена: {price} лари\n\n'
+        for day_of_week in DAILY:
+            if day_of_week in description:
+                flag = True
+                DAILY[day_of_week] += result
+        if not flag:
+            no_tag += result
+    bot.send_photo(chat_id, open(f'photos/no_tag.png', 'rb'), reply_to_message_id=2, parse_mode='html')
+    bot.send_message(chat_id, f'{no_tag}\n\n{FINISH_TEXT}', reply_to_message_id=2, parse_mode='html')
+    for day_of_week in DAILY:
+        if DAILY[day_of_week]:
+            photo = open(f'photos/{day_of_week}.jpg', 'rb')
+            bot.send_photo(chat_id, photo, f'{DAILY[day_of_week]}\n\n{FINISH_TEXT}',
+                           reply_to_message_id=2, parse_mode='html')
 
 
-sched = BlockingScheduler(timezone=get_localzone())
-sched.add_job(send_event_map, 'cron', day_of_week='mon', hour=9)
-sched.start()
+send_event_map()
+
+# sched = BlockingScheduler(timezone=get_localzone())
+# sched.add_job(send_event_map, 'cron', day_of_week='mon', hour=9)
+# sched.start()
